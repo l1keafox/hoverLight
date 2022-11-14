5e8034fbfb9a02e6272f3905f653ba2bb9598c07
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-14 16:34:09 -0700

    single card working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import { useEffect,useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [mousePos, setMousePos] = useState({});
-  function updateMouse(e){
-    const {currentTarget: target} = e;
+  function updateMouse(e) {
+    const { currentTarget: target } = e;
     setMousePos({
       ...mousePos,
-      "x":e.nativeEvent.screenX,
-      "y":e.nativeEvent.screenY,
+      x: e.nativeEvent.screenX,
+      y: e.nativeEvent.screenY,
     });
-    target.style.setProperty("--mouse-y",e.nativeEvent.offsetY+"px");
-    target.style.setProperty("--mouse-x",e.nativeEvent.offsetX+"px");
+    target.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
+    target.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
   }
-  
+
+  // function updateAllMouse(e){
+  //   for(const card of document.getElementsByClassName("card")) {
+  //       console.log('all');
+  //       card.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
+  //       target.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
+  //       }
+  // }
 
   return (
-    <div id="cards">
-        <div className = 'card' onMouseMove={updateMouse}></div>
-        <div className = 'card' onMouseMove={updateMouse} ></div>
-        <div className = 'card' onMouseMove={updateMouse}></div>
-        <div className = 'card' onMouseMove={updateMouse}></div>
-        <div className = 'card' onMouseMove={updateMouse}></div>
-        <div className = 'card' onMouseMove={updateMouse}></div>
-
-
+    <div id="cards" >
+      <div className="card" onMouseMove={updateMouse}>
+        <div className="card-border"> </div>
+        <div className="card-content"> </div>
+      </div>
+      <div className="card" onMouseMove={updateMouse}>
+      <div className="card-border"> </div>        
+        <div className="card-content"> </div>
+      </div>
+      <div className="card" onMouseMove={updateMouse}>
+      <div className="card-border"> </div>
+        <div className="card-content"> </div>
+      </div>
+      <div className="card" onMouseMove={updateMouse}>
+      <div className="card-border"> </div>
+        <div className="card-content"> </div>
+      </div>
+      <div className="card" onMouseMove={updateMouse}>
+      <div className="card-border"> </div>
+        <div className="card-content"> </div>
+      </div>
+      <div className="card" onMouseMove={updateMouse}>
+      <div className="card-border"> </div>
+        <div className="card-content"> </div>
+      </div>
     </div>
   );
 }
