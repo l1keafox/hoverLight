011fc9bed47d34cbecb49457523772a7fd302d86
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-14 15:52:38 -0700

    hover works!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import { useEffect,useState, CSSProperties } from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
   const [mousePos, setMousePos] = useState({});
-  let style1;
   function updateMouse(e){
     const {currentTarget: target} = e;
-    console.log('enter');
-    console.log(target,mousePos);
-    console.log(e.nativeEvent);
     setMousePos({
       ...mousePos,
       "x":e.nativeEvent.screenX,
       "y":e.nativeEvent.screenY,
     });
-    target.style.setProperty("--mouse-y",e.nativeEvent.screenY);
-    target.style.setProperty("--mouse-x",e.nativeEvent.screenX);
-    console.log("AFDESF",target);
+    target.style.setProperty("--mouse-y",e.nativeEvent.offsetY+"px");
+    target.style.setProperty("--mouse-x",e.nativeEvent.offsetX+"px");
   }
   
 
   return (
     <div id="cards">
-        <div className = 'card' onMouseMove={updateMouse} style = {style1}></div>
+        <div className = 'card' onMouseMove={updateMouse}></div>
         <div className = 'card' onMouseMove={updateMouse} ></div>
         <div className = 'card' onMouseMove={updateMouse}></div>
         <div className = 'card' onMouseMove={updateMouse}></div>
