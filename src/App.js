80fcc9a11401966ba5a43f3ee636910248518699
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-14 16:37:00 -0700

    mulit

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import "./App.css";
 function App() {
   const [mousePos, setMousePos] = useState({});
   function updateMouse(e) {
-    const { currentTarget: target } = e;
-    setMousePos({
-      ...mousePos,
-      x: e.nativeEvent.screenX,
-      y: e.nativeEvent.screenY,
-    });
-    target.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
-    target.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
+    // const { currentTarget: target } = e;
+    // setMousePos({
+    //   ...mousePos,
+    //   x: e.nativeEvent.screenX,
+    //   y: e.nativeEvent.screenY,
+    // });
+    // target.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
+    // target.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
   }
 
-  // function updateAllMouse(e){
-  //   for(const card of document.getElementsByClassName("card")) {
-  //       console.log('all');
-  //       card.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
-  //       target.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
-  //       }
-  // }
+  function updateAllMouse(e){
+    for(const card of document.getElementsByClassName("card")) {
+        console.log('all');
+        card.style.setProperty("--mouse-y", e.nativeEvent.offsetY + "px");
+        card.style.setProperty("--mouse-x", e.nativeEvent.offsetX + "px");
+    }
+  }
 
   return (
-    <div id="cards" >
+    <div id="cards" onMouseMove={updateAllMouse} >
       <div className="card" onMouseMove={updateMouse}>
         <div className="card-border"> </div>
         <div className="card-content"> </div>
