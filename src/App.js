3b9275bb34a6845048c6c1b2c9f35b2be885388d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-14 13:03:56 -0700

    ummm mouse event not working as intended

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function updateMouse(e){
+    const {currentTarget: target} = e;
+    console.log(target);
+    console.log(e.nativeEvent);
+    console.log('enter');
+    target.style.setProperty("--mouse-x",e.nativeEvent.screenX);
+    target.style.setProperty("--mouse-y",e.nativeEvent.screenY);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="cards">
+        <div className = 'card' onMouseEnter={ updateMouse } ></div>
+        <div className = 'card'></div>
+        <div className = 'card'></div>
+        <div className = 'card'></div>
+        <div className = 'card'></div>
+        <div className = 'card'></div>
+
+
     </div>
   );
 }
